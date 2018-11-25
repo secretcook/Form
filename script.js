c993a1b54ed041566f35a710be6c09ddf2785c74
secretcook <ags118118@gmail.com>
2018-11-25 12:21:36 +0700

    Artem

---
+++ script.js
@@ -0,0 +1,9 @@
+function submitAction(form){
+	var ima = document.getElementById("ima");
+	ima.innerHTML = form.name.value;
+   var language = document.getElementById("language");
+	language.innerHTML = form.name.value;
+	var password = document.getElementById("password");
+	password.innerHTML = form.name.value;
+   
+}
